8190748031426343b848b9a706a200dfc9deb195
davep777 <164636436+davep777@users.noreply.github.com>
2025-06-23 12:50:50 +0200

    Update zadanie_2_27_04.py

--- zadanie_2_27_04.py
+++ zadanie_2_27_04.py
@@ -1,56 +1,37 @@
 import re
 
-def count_words_by_letter(text: str) -> dict:
-    """
-    Funkcja zlicza liczbę słów zaczynających się na daną literę.
+# HashMapa
+class HashMapa:
+    def __init__(self, rozmiar=26):  # 26 liter alfabetu
+        self.rozmiar = rozmiar
+        self.tablica = [0] * rozmiar  # tablica dla liter
     
-    Krok 1: Tekst zostaje zamieniony na małe litery.
-    Krok 2: Usuwamy interpunkcję (wszystko, co nie jest literą, cyfrą lub białym znakiem).
-    Krok 3: Dzielimy tekst na słowa.
-    Krok 4: Tworzymy hashmapę (słownik), która dla każdej litery (pierwszy znak słowa) inkrementuje licznik.
-    Krok 5: Sortujemy wyniki malejąco wg liczby słów.
-    """
-    # Konwersja na małe litery
-    text = text.lower()
-    # Usuwanie interpunkcji – usuwamy wszystko co nie jest alfanumeryczne lub białym znakiem
-    text = re.sub(r"[^\w\s]", "", text)
-    # Dzielenie tekstu na słowa
-    words = text.split()
+    def hash(self, klucz):
+        return sum(ord(znak) for znak in klucz) % self.rozmiar  # służy za index
+
+    def dodaj(self, klucz):
+        indeks = self.hash(klucz)  # oblicza indeks
+        self.tablica[indeks] += 1  # zwieksza licznik dla litery
+
+    def posortowane(self):
+        litery = [chr(i + ord('a')) for i in range(self.rozmiar)]  # lista liter a-z 0+97, 0+98 itd.
+        liczby = list(zip(litery, self.tablica))  # laczmy litery z liczba wystapien
+        return sorted(liczby, key=lambda x: x[1], reverse=True)  # sortujemy
+
+# Funkcja liczaca slowa w tekscie
+def licz_slowa_po_literze(tekst):
+    hash_mapa = HashMapa()
+    slowa = re.findall(r'\b\w+\b', tekst.lower())  # usuwa interpunkcje i tworzy małe litery
     
-    counts = {}
-    # Liczymy słowa zaczynające się na literę (pomijamy te, które zaczynają się np. cyfrą)
-    for word in words:
-        if word:  # zabezpieczenie przed pustymi ciągami
-            first_letter = word[0]
-            if first_letter.isalpha():
-                counts[first_letter] = counts.get(first_letter, 0) + 1
+    for slowo in slowa:
+        pierwsza_litera = slowo[0]  # pobiera pierwszą literę
+        hash_mapa.dodaj(pierwsza_litera)
     
-    # Sortujemy słownik wg liczby wystąpień malejąco
-    sorted_counts = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
-    return sorted_counts
+    return hash_mapa.posortowane()  # sortowanie
 
-# Wygenerowany tekst o długości około 634 słów:
-text = """
-Tu w sercu malowniczego regionu, gdzie zielone łąki rozciągają się niczym bajkowa opowieść, stanęła niewielka osada, której mieszkańcy znani byli z serdeczności i niezwykłego gościnności. Pewnego słonecznego poranka, gdy mgła jeszcze leniwie unosiła się nad dolinami, zebrali się ludzie, aby rozpocząć dzień pełen nadziei i nowych wyzwań. Każdy z nich miał swoją historię, którą opowiadali z pasją przy ognisku, gdy ciepło ognia łączyło ich w jedną wielką rodzinę.
+# Test z przykladowym tekstem
+tekst = """Lorem ipsum dolor sit amet. Ad alias odio ea rerum porro At modi quisquam. Quo neque dolorem ut quaerat dicta a Quis autem eum necessitatibus facere 33 quasi fuga aut ullam commodi. Et quia modi qui sequi magni eum unde iste cum accusantium pariatur! Ut quae cumque aut numquam ipsum aut incidunt autem. Aut culpa voluptas 33 deleniti temporibus et officiis galisum est aperiam aperiam est quasi vitae. Aut adipisci odit ab deserunt nulla qui quibusdam sunt aut nemo commodi aut maiores suscipit. Aut earum facere eum optio vero et beatae voluptatem. Ut eaque voluptas est nostrum voluptates ut omnis eaque id molestiae internos.
+Sed rerum debitis ab velit porro ea voluptatem reiciendis a consequatur tenetur id galisum atque ut culpa perferendis? Eos voluptatum velit vel maiores rerum non sequi beatae sed quaerat laudantium non optio suscipit. Non porro galisum ut modi delectus et rerum quibusdam et mollitia praesentium et quas corrupti ut error soluta. Est velit nihil sed rerum deserunt aut dolores ipsa qui velit voluptatem. A exercitationem possimus non dolorem magnam et minus voluptatum nam aliquam nemo sit nisi omnis id necessitatibus voluptatum. Est illum fugiat ut magnam ipsa id ducimus dolores eos dolores tenetur vel consectetur accusamus aut modi dolor. Hic quia perspiciatis 33 mollitia saepe id exercitationem fugiat nam labore minima qui veniam autem est tempora cupiditate! Et libero dolorem a accusantium possimus id reprehenderit omnis ea dignissimos dolor et provident maxime et nemo dolor. Est aliquid debitis ex amet Quis ut quibusdam dolorem. Sit eius eaque ut consequuntur beatae sed voluptatem sunt cum tempore iste id temporibus nobis est nihil dolorum. Ut eveniet quia id quod neque hic quasi maxime aut animi voluptatibus ad eveniet minima sed illo ipsam. Aut magni facere ut culpa omnis sed tenetur impedit est totam enim et voluptatem delectus. Sed enim culpa ab consectetur expedita aut nostrum dicta aut incidunt voluptas ab rerum assumenda et consequatur dignissimos. In ducimus porro sed consequatur perspiciatis ad architecto accusamus! """
+liczba_slow = licz_slowa_po_literze(tekst)
+print(liczba_slow)
 
-Dzień zaczynał się od dźwięku dzwonów, które budziły mieszkańców, zwiastując początek pracy na polach. Farmaceuci, kowale i nauczyciele – wszyscy znajdowali swoje miejsce w tej harmonijnej społeczności. Zarówno młodzi, jak i starzy wiedzieli, że każdy dzień niesie ze sobą nowe możliwości i przygody. Pracowite dusze stawiały czoła trudom życia, wierząc, że wszelkie przeszkody można przezwyciężyć, jeśli tylko człowiek z odwagą i determinacją podąża za swoim marzeniem.
-
-Przygoda w tej osadzie nie ograniczała się tylko do codziennej pracy – wieczory były czasem, gdy opowiadano historie o dawnych czasach, legendach przekazywanych z pokolenia na pokolenie. W opowieściach tych pojawiały się mityczne postacie, czyli bohaterowie i bohaterki, które swoim przykładem uczyły, jak być prawdziwymi ludźmi honoru i wartości. Legenda o dzielnym rycerzu, który w obronie słabszych walczył z niesprawiedliwością, umacniała ducha całej wspólnoty. Każda opowieść motywowała do działania i dawała nadzieję na lepszą przyszłość.
-
-Nad brzegiem rzeki, gdzie woda szepcze swoje sekrety, znajdował się stary dąb, który świadczył o przemijających czasach. Niezliczone wieki minęły, od kiedy ten dąb po raz pierwszy zakorzenił się w bogatej glebie, a on sam stał się świadkiem ludzkich losów. Przez tygodnie i miesiące, podobnie jak życie, dąb widział radość i smutek, śmiech i łzy, a też ciepło letnich wieczorów i chłód mroźnych nocy. Cisza tego miejsca przyciągała poszukiwaczy spokoju, którzy znaleźli w nim ukojenie i zadumę.
-
-W małej, przytulnej kawiarence, gdzie zapach świeżo parzonej kawy unosił się w powietrzu, spotykali się mieszkańcy, aby dzielić się swoimi doświadczeniami, anegdotami i marzeniami. Barista zawsze pamiętał o ulubionych napojach swoich gości i serwował je z uśmiechem, który rozświetlał nawet najciemniejsze dni. Każdy, kto przekraczał próg tego miejsca, czuł, że jest częścią czegoś większego, że wspólnota jest siłą, która potrafi połączyć różnorodne losy.
-
-Z dala od zgiełku wielkich miast, ta osada była oazą dla zmęczonych dusz, które szukały wytchnienia i autentycznego kontaktu z naturą. Piękne krajobrazy, w których łąki przeplatały się z głębokimi lasami, tworzyły scenerię równie niezwykłą, jak opowieści snute przez starych mędrców. Pokochać to miejsce potrafił każdy, kto choć raz tutaj zawitał, a wspomnienia z tych dni pozostawały na zawsze w pamięci, niezależnie od upływu czasu.
-
-Podczas zimowych dni mieszkańcy osady organizowali liczne festyny i jarmarki, gdzie tradycje i rękodzieło były celebrowane z dumą oraz radością. Kramy wypełnione były świeżymi owocami, ręcznie robionymi wyrobami oraz opowieściami, które zdawały się przenosić uczestników w zupełnie inny wymiar, pełen magii i stworzonej z serca sztuki. Kolejne pokolenia uczyły się od starszych, słuchając z zapartym tchem każdej historii i czerpiąc inspirację z mądrości przekazywanej przez czas.
-
-Pomiędzy pracą a odpoczynkiem, życie płynęło tu niczym spokojna rzeka, której bieg odzwierciedlał rytm serc mieszkańców. Codziennie wschodziło słońce, przynosząc nową energię i przypominając, że każdy dzień jest wyjątkowy. Z pokorą i radością witano każdy poranek, wiedząc, że świat pełen jest cudów, a miłość i dobroć są wartościami, które nigdy nie wyjdą z mody. Takie podejście do życia sprawiało, że nawet najmniejsze wyzwania stawały się okazją do nauki i osobistego rozwoju.
-
-Między fragmentami tej opowieści można odnaleźć głębsze przesłanie; każdy, kto był częścią tej wspólnoty, wiedział, że życie jest piękne i pełne niespodzianek. Odwaga, pracowitość oraz wzajemny szacunek były fundamentem, na którym budowano codzienność. Mieszkańcy osady wierzyli w siłę jedności, wiedząc, że razem potrafią przezwyciężyć wszelkie przeciwności, a każda przygoda stawała się kolejnym rozdziałem w ich wspólnym życiu. Dzięki temu każdy dzień miał swój unikalny charakter, a historia tej małej społeczności zapisywała się złotymi literami, pozostawiając niezatarty ślad w sercach wszystkich, którzy ją odwiedzili.
-"""
-
-# Wywołanie funkcji i wyświetlenie wyników
-stats = count_words_by_letter(text)
-print("Liczba słów zaczynających się na daną literę (posortowane od najwięcej do najmniej):")
-print(stats)
